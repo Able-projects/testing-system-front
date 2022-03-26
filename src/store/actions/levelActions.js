d2a496a59efc17db4ab1fdb28e3eb910529969a3
Ками Жаудатов <kamilzaudatov@gmail.com>
2022-03-26 19:57:30 +0600

    level

--- src/store/actions/levelActions.js
+++ src/store/actions/levelActions.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { SET_LEVEL_LIST } from "./types"
+import { SET_LEVEL_LIST, SET_ERRORS } from "./types"
 import { setAuthToken } from './authActions'
 export const getLevelList = () => dispatch => {
     setAuthToken()
@@ -7,6 +7,48 @@ export const getLevelList = () => dispatch => {
         dispatch({
             type: SET_LEVEL_LIST,
             payload: res.data?.data
+        })
+    }).catch(() => {
+        dispatch({
+            type: SET_LEVEL_LIST,
+            payload: null
+        })
+    } )
+}
+
+export const deleteLevels = (id) => dispatch => {
+    setAuthToken()
+    axios.delete('http://localhost:5050/api/level/'+id).then(res => {
+        dispatch(getLevelList())
+    })
+}
+
+export const addLevel = (data,handleClose) => dispatch => {
+    setAuthToken()
+    axios.post('http://localhost:5050/api/level/',data).then(res => {
+        if(res.data.success){
+            handleClose()
+            dispatch(getLevelList())
+        }
+    }).catch(error => {
+        dispatch({
+            type: SET_ERRORS,
+            payload: error?.response?.data
+        })
+    })
+}
+
+export const editLevels = (id,data,handleClose) => dispatch => {
+    setAuthToken()
+    axios.put('http://localhost:5050/api/level/'+id,data).then(res => {
+        if(res.data.success){
+            handleClose()
+            dispatch(getLevelList())
+        }
+    }).catch(error => {
+        dispatch({
+            type: SET_ERRORS,
+            payload: error?.response?.data
         })
     })
 }
