cd41f9766468542bfe8f5610c21f1325f2cdb130
Arman-Zh <lunatik84@bk.ru>
2022-04-23 20:08:43 +0600

    testPage

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -5,33 +5,49 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Test from '../test/Test.js'
 
 export default function TestCard(props) {
+  // добавить остальные типы карточек
   const getImage = () => {
-    if(props.title === 'HTML'){
+    if (props.title === 'HTML') {
       return "/Image/html/icons8-html-5-150.svg"
     }
-    return "/Image/html/icons8-html-5-150.svg"
+    if (props.title === 'CSS') {
+      return "/Image/css/icons8-css3-150.svg"
+    }
+    if (props.title === 'JS') {
+      return "Image/javascript/icons8-javascript-150.svg"
+    }
+    if (props.title === 'React') {
+      return "Image/react/icons8-react-native-150.svg"
+    }
+    return "/Image/javascript-html5-css3.png"
+    // добавить номальную все картинки в одной
   }
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        component="img"
-        height="220"
-        image={getImage()}
-        alt="green iguana"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          sx={{ width: 220, m: 'auto' }}
+          image={getImage()}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
             {props.title}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
             {props.level}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Button size="small">Начать</Button>
-      </CardActions>
-    </Card>
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Начать</Button>
+        </CardActions>
+      </Card>
+      <Test />
+    </>
+
   );
 }
